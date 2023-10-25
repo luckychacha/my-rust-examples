603ceaa7d9b976791613edccee827b30bfce9c7b
luckychacha <358140284@qq.com>
2023-10-25 17:54:01 +0800

    Create Code Wars workspace.

---
+++ code-wars/examples/to_camel_case.rs
@@ -0,0 +1,17 @@
+pub fn to_camel_case(text: &str) -> String {
+	if text.is_empty() {
+		return text.to_owned();
+	}
+	text.split(&['-', '_'])
+		.enumerate()
+		.map(|(idx, item)| match idx {
+			0 => item.to_owned(),
+			_ => item[..1].to_ascii_uppercase() + &item[1..],
+		})
+		.collect()
+}
+
+fn main() {
+	let text = "the-stealth-warrior";
+	println!("{}", to_camel_case(text));
+}
